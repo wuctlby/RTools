022b086c1e836cef3350f523cada8b259eeba512
Chuntai <wulibeiyu@gmail.com>
2025-09-22 10:03:00 +0200

    sub-shell

--- Linux/script/create_tbrowser.sh
+++ Linux/script/create_tbrowser.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 BASH_DIR=$(dirname "$0")
-Target_Bash_Dir=$(realpath "$BASH_DIR/../src/OpenNewTBrowser.sh")
-echo "Target Bash Script: $Target_Bash_Dir"
 
 cat << EOL > ~/.local/share/applications/root-tbrowser.desktop
 [Desktop Entry]
 Version=1.0
 Type=Application
 Name=ROOT TBrowser
-Exec=gnome-terminal -- bash "$Target_Bash_Dir" %f
+Exec=gnome-terminal -- bash /home/wuct/ALICE/reps/RTools/Linux/src/OpenNewTBrowser.sh %f
 Icon=utilities-terminal
 Terminal=false
 # MIME type for ROOT files, used to associate .root files with this application
--- Linux/src/OpenNewTBrowser.sh
+++ Linux/src/OpenNewTBrowser.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# source $HOME/Software/root/bin/thisroot.sh
+
 if [ $# -lt 1 ]; then
   echo "Usage: $0 <ROOT_FILE>"
   exit 1
@@ -10,7 +12,22 @@ if [[ "$1" != *.root ]]; then
     exit 1
 fi
 
-cd "$(dirname "$1")" || exit 1
-root --web=off -l -e "TFile::Open(\"$1\"); new TBrowser(\"$1\");"
+(
+  source $HOME/Software/root/bin/thisroot.sh
+  cd "$(dirname "$1")" || exit 1
+  root --web=off -l -e "TFile::Open(\"$1\"); new TBrowser(\"$1\");"
+)
+
+# cd "$(dirname "$1")" || exit 1
+# root --web=off -l -e "TFile::Open(\"$1\"); new TBrowser(\"$1\");"
+
+# while true; do
+#     read -rp "Do you want to close the terminal? (y/n): " yn
+#     case $yn in
+#         [Yy]* ) break;;
+#         [Nn]* ) echo "Keeping the terminal open. Type 'exit' to close it."; bash;;
+#         * ) echo "Please answer yes or no.";;
+#     esac
+# done
 
 exit 0
